75792f9db662ca2d60e0aade61745a1f9803f7d1
Kushal Kongara <kkushal2509@gmail.com>
2025-08-05 14:46:38 -0500

    Added endpoints in the backend

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  deleteNote,
+  getAllNotes,
+  createNote,
+  updateNote,
+} from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+export default router;
